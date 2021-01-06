65cb7e8ea9b69879c177d667d7a119dbd880570f
shivanjaywagh <wagh.shivanjay099@gmail.com>
2021-01-07 01:10:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('heart_disease_random_forest_model.pkl', 'rb'))
+
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
@@ -30,35 +31,10 @@ def predict():
         Restecg = int(request.form['restecg'])
         Thalach = int(request.form['thalach'])
         Exang = int(request.form['exang'])
-
         Oldpeak=float(request.form['oldpeak'])
-        
-        
         Slope=int(request.form['slope'])
         Ca=int(request.form['slope'])
         Thal=int(request.form['slope'])
-        
-        #Kms_Driven2=np.log(Kms_Driven)
-        #Owner=int(request.form['Owner'])
-        #Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        #if(Fuel_Type_Petrol=='Petrol'):
-        #        Fuel_Type_Petrol=1
-        #        Fuel_Type_Diesel=0
-        #else:
-        #    Fuel_Type_Petrol=0
-        #    Fuel_Type_Diesel=1
-        #Year=2020-Year
-        #Gender=request.form['Gender']
-        #if(Gender=='Male'):
-        #    Gender=1
-        #else:
-        #    Gender=0	
-            
-        #Transmission_Mannual=request.form['Transmission_Mannual']
-        #if(Transmission_Mannual=='Mannual'):
-        #    Transmission_Mannual=1
-        #else:
-        #    Transmission_Mannual=0
         prediction=model.predict([[Age,sex,cp,tresttbps,chol,fbs,restecg,thalach,exang,oldspeak,slope,ca,thal]])
         output=round(prediction[0],2)
         if output==0:
