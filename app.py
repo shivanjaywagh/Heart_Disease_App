8efdd971cc877cab10696e40c32480b190c0aa2c
shivanjaywagh <wagh.shivanjay099@gmail.com>
2021-01-07 02:18:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def predict():
         Ca=int(request.form['ca'])
         Thal=int(request.form['thal'])
         prediction=model.predict([[Age,Gender,Cp,Tresttbps,Chol,Fbs,Restecg,Thalach,Exang,Oldpeak,Slope,Ca,Thal]])
-        output=round(prediction[0],2)
+        output=prediction
         if output==0:
             return render_template('index.html',prediction_text="Patient does not have heart disease")
         elif output==1:
